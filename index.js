ce7d7ed379f3f908b98ec2698a2eb662b7bfc2ab
Erin Penner <epenner0522@gmail.com>
2018-08-06 14:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,22 +22,17 @@ function deepestChild() {
   // select the element that's a div with the id 'grand-node'
   let sel = document.querySelector('div#grand-node');
   
+  // Get the node's children
   let nextChild = sel.children;
 
+  /* only looking at the first child at each level, check that the 
+     first child at the next level exists. If it doesn't break out of the loop, knowing we're at the last level
+     If it does, set the next child to the children of the current level
+  */ 
   while (true) {
     if (nextChild[0].children[0] === undefined) break;
     nextChild = nextChild[0].children;
-    console.log(nextChild[0].innerHTML);
   }
-
-  console.log("Compare results");
-  console.log(document.querySelector('#grand-node div div div div'));
-  console.log(nextChild[0]);
   return nextChild[0];
 }
 
-//console.log(getFirstSelector('div').id);
-//console.log(getFirstSelector('.ranked-list'));
-//console.log(nestedTarget());
-//console.log(increaseRankBy(3));
-console.log (thing = deepestChild());
